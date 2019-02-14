1f9e205ccf18eaea0da0f8b332c1d6c60ec3246f
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2019-02-13 21:55:23 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,8 +8,14 @@ prefix = ("J")
 client = Bot(prefix)
 
 @client.command()
-async def test():
-   await client.say("success")
+async def website():
+   '''Server Website'''
+   await client.say("http://www.jellonation.ga")
+
+@client.command()
+async def donate():
+   '''Server Donations'''
+   await client.say("Donations Are Disabled!")
 
 
 client.run("NDY5MjYzMjQwMDAyNzk3NTkw.D0ZmSg.oLpee2XRENyijvL0ZV4pTdP-E4o")
